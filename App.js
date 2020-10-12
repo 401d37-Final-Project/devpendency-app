d134134be2341d5ab195fe8832123e25a3619657
Matt Herriges <herrigesmt@gmail.com>
2020-10-12 11:42:09 -0700

    playing with sketch module

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Sketch from './sketch'
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Can you see this change?</Text>
+      <Sketch />
       <StatusBar style="auto" />
     </View>
   );
