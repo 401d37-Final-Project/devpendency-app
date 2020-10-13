4bced1b1973f3adb3a76f55e6c24e0366f54e1bf
blakerom <blakeromero001@gmail.com>
2020-10-12 19:54:03 -0700

    modularizing code

--- App.js
+++ App.js
@@ -1,81 +1,111 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native';
 import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+import Constants from 'expo-constants';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-// const peopleObjects = [{name: "Blake", subtitle: "developer"},{name: "Tia", subtitle: "software developer"}, {name: "Matt", subtitle: "software dev"}, {name: "Stephen", subtitle: "dev"}];
-
-export default function People() {
-  const peopleObjects = [
-    {
-      id: 1,
-      name: "Blake", 
-      subtitle: "Software Developer",
-      content: "Card content 1"
-    },
-    {
-      id: 2,
-      name: "Tia", 
-      subtitle: "Software Developer",
-      content: "Card content 2"
-    }, 
-    {
-      id: 3,
-      name: "Matt", 
-      subtitle: "Software Developer",
-      content: "Card content 3"
-    }, 
-    {
-      id: 4,
-      name: "Stephen", 
-      subtitle: "Software Developer",
-      content: "Card content 4"
-    }
-  ];
-
-  const renderCard = () => {
-    return (peopleObjects.map(people => {
-      return (
-      <>
-        <Card>
-          <Card.Title title={people.name} subtitle={people.subtitle} />
-          <Card.Content>
-          </Card.Content>
-          <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-            <Paragraph>{people.content}</Paragraph>
-
-        
-            <Button>
-              <Icon.Button
-                name="github"
-                backgroundColor="#2b3137"
-                // onPress={this.gotoGithub}
-              >
-              Check my Github
-              </Icon.Button>
-            </Button>
-
-            <Button>
-              <Icon.Button
-                name="linkedin"
-                backgroundColor="#0e76a8"
-                // onPress={this.gotoLinkedin}
-              >
-              Connect on Linkedin
-              </Icon.Button>
-            </Button>
-
-        </Card>
-      </>
-
-    );
-    }));
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+  scrollView: {
+    backgroundColor: 'green',
+    marginHorizontal: 20,
+  },
+  iconButton: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   }
-  
-  return(
-    <View>
-    {renderCard()}
-    </View>
-    )
+});
+
+const peopleObjects = [
+  {
+    id: 1,
+    name: 'Blake', 
+    subtitle: 'Software Developer',
+    content: 'Card content 1',
+    uri: 'https://picsum.photos/700'
+  },
+  {
+    id: 2,
+    name: 'Tia', 
+    subtitle: 'Software Developer',
+    content: 'Card content 2',
+    uri: 'https://picsum.photos/700'
+  }, 
+  {
+    id: 3,
+    name: 'Matt', 
+    subtitle: 'Software Developer',
+    content: 'Card content 3',
+    uri: 'https://picsum.photos/700'
+  }, 
+  {
+    id: 4,
+    name: 'Stephen', 
+    subtitle: 'Software Developer',
+    content: 'Card content 4',
+    uri: 'https://picsum.photos/700'
+  }
+];
+
+const gotoGithub = () => {
+  console.log('clicked on Github');
 }
+
+const RenderCard = props => {
+  return (
+    <View>
+      <Card>
+        <Card.Title title={props.people.name} subtitle={props.people.subtitle} />
+        <Card.Content>
+        </Card.Content>
+        <Card.Cover source={{ uri: props.people.uri }} />
+          <Paragraph>{props.people.content}</Paragraph>
+
+          <Icon.Button style={styles.iconButton}
+            name='github'
+            backgroundColor='#2b3137'
+            onPress={gotoGithub()}
+          >
+          Check my Github
+          </Icon.Button>
+
+          <Icon.Button style={styles.iconButton}
+            name='linkedin'
+            backgroundColor='#0e76a8'
+            // onPress={this.gotoLinkedin}
+          >
+          Connect on Linkedin
+          </Icon.Button>
+
+      </Card>
+    </View>
+  );
+};
+
+const RenderCardList = () => {
+  return (
+      peopleObjects.map(people => {
+        return <RenderCard people={people} key={people.id} />;
+      })
+  );
+};
+
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+      
+      <RenderCardList />
+      
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+//Thanks to this guide, it helped out a lot!! https://dev.to/britneys/creating-reusable-react-components-with-map-4823
