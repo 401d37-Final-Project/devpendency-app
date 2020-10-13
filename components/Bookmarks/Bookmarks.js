4be0403d6e39ec0c74beea45cdc45250033ebc6f
SBALDOCK <stephenbaldock78@gmail.com>
2020-10-13 14:33:35 -0700

    styling bookmarks

--- components/Bookmarks/Bookmarks.js
+++ components/Bookmarks/Bookmarks.js
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet, FlatList, Text, TextInput, View, Button, Linking, TouchableOpacity } from 'react-native';
+import { StyleSheet, SafeAreaView, ScrollView, StatusBar, FlatList, Text, Container, TextInput, View, Button, Linking, TouchableOpacity } from 'react-native';
 import { Card, ListItem, Paragraph, Icon } from 'react-native-elements';
 import { useFormik, Formik, Field, Form } from 'formik';
 import { color } from 'react-native-reanimated';
@@ -14,8 +14,26 @@ const styles = StyleSheet.create({
     margin: 15,
     borderColor: 'black',
     borderWidth: 1,
-    width: 500
+    width: 275
   },
+  baseText: {
+    fontFamily: "Cochin",
+  },
+  heading: {
+    fontFamily: "Cochin",
+    textAlign: 'center',
+    marginBottom: 30
+  },
+  description: {
+    fontFamily: "Cochin",
+    marginTop: 20,
+    textAlign: 'center',
+  },
+  delete: {
+    fontFamily: "Cochin",
+    textAlign: 'right',
+  }
+
 })
 
 const Bookmarks = (props) => {
@@ -33,58 +51,72 @@ const Bookmarks = (props) => {
 
     return (
       <>
-        <TouchableOpacity
-          keyExtractor={(item) => item.id}
-          item={item}>
-          <Button title={item.values.name} onPress={handleClick}></Button>
-        </TouchableOpacity>
-        <Text>{item.values.description}</Text>
+        <Card>
+          <TouchableOpacity
+            keyExtractor={(item) => item.id}
+            item={item}>
+            <Button
+
+              title={item.values.name}
+              onPress={handleClick}
+            />
+          </TouchableOpacity>
+          <Text style={styles.description}>
+            {item.values.description}
+          </Text>
+          <Text style={styles.delete}>
+            Delete
+          </Text>
+        </Card>
       </>
     )
   }
   return (
     <>
-      <Formik
-        initialValues={{
-          name: '',
-          url: '',
-          description: ''
-        }}
-        onSubmit={(values, { resetForm }) => {
-          console.log('submitted', values)
-          setBookmarks([...bookmark, { values }]);
-          resetForm({ values: '' })
-        }
+      <Card>
+        <Formik
+          initialValues={{
+            name: '',
+            url: '',
+            description: ''
+          }}
+          onSubmit={(values, { resetForm }) => {
+            console.log('submitted', values)
+            setBookmarks([...bookmark, { values }]);
+            resetForm({ values: '' })
+          }
 
-        }>
+          }>
 
-        {({ handleChange, handleSubmit, values }) => (
-          <View>
-            <Text>Bookmark URL</Text>
-            <TextInput
-              style={styles.input}
-              value={values.url}
-              onChangeText={handleChange('url')} />
-            <Text>Bookmark Name</Text>
-            <TextInput
-              style={styles.input}
-              value={values.name}
-              onChangeText={handleChange('name')} />
-            <Text>Bookmark Description</Text>
-            <TextInput
-              style={styles.input}
-              value={values.description}
-              onChangeText={handleChange('description')} />
-            <Button
-              onPress={handleSubmit}
-              title="submit"
-              color="gray" />
-
-          </View>
-        )}
-      </Formik>
+          {({ handleChange, handleSubmit, values }) => (
+            <View>
+              <Text style={styles.heading} >Add a new Bookmark</Text>
+              <Text style={styles.baseText}>Bookmark URL</Text>
+              <TextInput
+                style={styles.input}
+                value={values.url}
+                onChangeText={handleChange('url')} />
+              <Text style={styles.baseText}>Bookmark Name</Text>
+              <TextInput
+                style={styles.input}
+                value={values.name}
+                onChangeText={handleChange('name')} />
+              <Text style={styles.baseText}>Bookmark Description</Text>
+              <TextInput
+                style={styles.input}
+                value={values.description}
+                onChangeText={handleChange('description')} />
+              <Button
+                onPress={handleSubmit}
+                title="submit"
+                color="gray" />
+            </View>
+          )}
+        </Formik>
+      </Card>
 
       <FlatList
+        style={{ marginTop: 40 }}
         data={bookmarkList}
         keyExtractor={(item) => item.id}
         renderItem={renderItem}
