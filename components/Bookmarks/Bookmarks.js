fbcc11236d24f420c5ec74249f1f4c1be5e157b3
SBALDOCK <stephenbaldock78@gmail.com>
2020-10-13 16:31:05 -0700

    styling with react paper

--- components/Bookmarks/Bookmarks.js
+++ components/Bookmarks/Bookmarks.js
@@ -1,11 +1,11 @@
 import 'react-native-gesture-handler';
 import React, { useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+// import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet, SafeAreaView, ScrollView, StatusBar, FlatList, Text, Container, TextInput, View, Button, Linking, TouchableOpacity } from 'react-native';
+import { StyleSheet, SafeAreaView, ScrollView, StatusBar, FlatList, Text, Container, TextInput, View, Linking, TouchableOpacity } from 'react-native';
 import { Card, ListItem, Paragraph, Icon } from 'react-native-elements';
 import { useFormik, Formik, Field, Form } from 'formik';
-import { color } from 'react-native-reanimated';
+import { DefaultTheme, Button, Provider as PaperProvider } from 'react-native-paper';
 
 const Stack = createStackNavigator();
 
@@ -36,6 +36,16 @@ const styles = StyleSheet.create({
 
 })
 
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#3498db',
+    accent: '#f1c40f',
+  },
+};
+
 const Bookmarks = (props) => {
 
   const [bookmark, setBookmarks] = useState([]);
@@ -48,87 +58,100 @@ const Bookmarks = (props) => {
     const handleClick = () => {
       Linking.openURL(`${item.values.url}`);
     }
-
     return (
-      <>
-        <Card>
-          <TouchableOpacity
-            keyExtractor={(item) => item.id}
-            item={item}>
-            <Button
-
-              title={item.values.name}
-              onPress={handleClick}
-            />
-          </TouchableOpacity>
-          <Text style={styles.description}>
-            {item.values.description}
+      <PaperProvider theme={theme}>
+        <>
+          <Card>
+            <TouchableOpacity
+              keyExtractor={(item) => item.id}
+              item={item}>
+              <Button
+                mode="outlined" onPress={handleClick}>
+                {item.values.name}
+              </Button>
+            </TouchableOpacity>
+            <Text style={styles.description}>
+              {item.values.description}
+            </Text>
+            <Text style={styles.delete}>
+              Delete
           </Text>
-          <Text style={styles.delete}>
-            Delete
-          </Text>
-        </Card>
-      </>
+          </Card>
+        </>
+      </PaperProvider>
     )
   }
   return (
-    <>
-      <Card>
-        <Formik
-          initialValues={{
-            name: '',
-            url: '',
-            description: ''
-          }}
-          onSubmit={(values, { resetForm }) => {
-            console.log('submitted', values)
-            setBookmarks([...bookmark, { values }]);
-            resetForm({ values: '' })
-          }
+    <PaperProvider theme={theme}>
+      <>
+        <Card>
+          <Formik
+            initialValues={{
+              name: '',
+              url: '',
+              description: ''
+            }}
+            onSubmit={(values, { resetForm }) => {
+              console.log('submitted', values)
+              setBookmarks([...bookmark, { values }]);
+              resetForm({ values: '' })
+            }
 
-          }>
+            }>
 
-          {({ handleChange, handleSubmit, values }) => (
-            <View>
-              <Text style={styles.heading} >Add a new Bookmark</Text>
-              <Text style={styles.baseText}>Bookmark URL</Text>
-              <TextInput
-                style={styles.input}
-                value={values.url}
-                onChangeText={handleChange('url')} />
-              <Text style={styles.baseText}>Bookmark Name</Text>
-              <TextInput
-                style={styles.input}
-                value={values.name}
-                onChangeText={handleChange('name')} />
-              <Text style={styles.baseText}>Bookmark Description</Text>
-              <TextInput
-                style={styles.input}
-                value={values.description}
-                onChangeText={handleChange('description')} />
-              <Button
-                onPress={handleSubmit}
-                title="submit"
-                color="gray" />
-            </View>
-          )}
-        </Formik>
-      </Card>
+            {({ handleChange, handleSubmit, values }) => (
 
-      <FlatList
-        style={{ marginTop: 40 }}
-        data={bookmarkList}
-        keyExtractor={(item) => item.id}
-        renderItem={renderItem}
-      />
+              <View>
+                <Text style={styles.heading} >Add a new Bookmark</Text>
+                <Text style={styles.baseText}>Bookmark URL</Text>
+                <TextInput
+                  style={styles.input}
+                  value={values.url}
+                  onChangeText={handleChange('url')} />
+                <Text style={styles.baseText}>Bookmark Name</Text>
+                <TextInput
+                  style={styles.input}
+                  value={values.name}
+                  onChangeText={handleChange('name')} />
+                <Text style={styles.baseText}>Bookmark Description</Text>
+                <TextInput
+                  style={styles.input}
+                  value={values.description}
+                  onChangeText={handleChange('description')} />
+                <Button
+                  mode="outlined"
+                  onPress={handleSubmit}
+                  color="gray">
+                  Submit
+              </Button>
+              </View>
 
-    </>
+            )}
+          </Formik>
+        </Card>
+
+        <FlatList
+          style={{ marginTop: 40 }}
+          data={bookmarkList.sort((a, b) => a.name - b.name)}
+          keyExtractor={(item) => item.id}
+          renderItem={renderItem}
+        />
+
+      </>
+    </PaperProvider>
   );
 
 };
 
 
 export default Bookmarks;
+
+
+// alphabetize results
+// add a delete button
+// style the page
+// nav on the bottom of the screen at all times
+// 
 
 
 
