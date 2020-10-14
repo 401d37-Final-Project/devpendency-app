7c81236612ad8b3c4c86dedd2a22dbef76c8bd14
blakerom <blakeromero001@gmail.com>
2020-10-13 21:59:32 -0700

    added fonts, icons on nav bar, styling

--- App.js
+++ App.js
@@ -3,7 +3,10 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { configureFonts, DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { Ionicons } from '@expo/vector-icons';
+// import Ionicons from 'react-native-vector-icons/Ionicons';
+
 
 import { StyleSheet, Text, View, Button, TouchableOpacity, Image } from 'react-native';
 import Bookmarks from './components/Bookmarks.js';
@@ -12,6 +15,19 @@ import Devnap from './components/Sketch.js';
 import AboutUs from './components/DevList.js';
 
 const Tab = createBottomTabNavigator();
+
+const fontConfig = {
+  default: {
+    // regular: {
+    //   fontFamily: 'sans-serif',
+    //   fontWeight: 'normal',
+    // },
+    medium: {
+      fontFamily: 'exo',
+      fontWeight: 'medium',
+    },
+  },
+};
 
 const theme = {
   ...DefaultTheme,
@@ -24,18 +40,21 @@ const theme = {
     background: '#2E373E',
     text: '#000',
   },
+  fonts: configureFonts(fontConfig),
 };
 
 export default function App() {
 
   const HomeScreen = ({ navigation }) => {
     return (
-      <>
+      // <>
 
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
 
-      <Text>Welcome Developer!</Text>
+      
+      <Text>Welcome, Developer to </Text>
       <Image 
-        source={require('./assets/logo.png')}
+        source={require('./assets/logo_dark.png')}
         style={styles.logo}/>
 
 
@@ -59,8 +78,8 @@ export default function App() {
         /> */}
 
 
-
-        </>
+</View>
+        // </>
 
     );
   };
@@ -72,15 +91,43 @@ export default function App() {
 
     <PaperProvider theme={theme}>
       <NavigationContainer theme={theme}>
-        <Tab.Navigator>
+        <Tab.Navigator
+                screenOptions={({ route }) => ({
+                  tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+        
+                    if (route.name === 'Home') {
+                      iconName = focused
+                        ? 'md-home'
+                        : 'ios-home';
+                    } else if (route.name === 'Napkin') {
+                      iconName = focused ? 'ios-list-box' : 'ios-list';
+                    } else if (route.name === 'Bookmarks') {
+                      iconName = focused ? 'ios-list-box' : 'md-book';
+                    } else if (route.name === 'Job Tracker') {
+                      iconName = focused ? 'ios-list-box' : 'md-document';
+                    } else if (route.name === 'About the Devs') {
+                      iconName = focused ? 'ios-list-box' : 'ios-person';
+                    }
+        
+                    // You can return any component that you like here!
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                  },
+                })}
+                tabBarOptions={{
+                  activeTintColor: '#F9665E',
+                  inactiveTintColor: '#000',
+                }}
+        >
           <Tab.Screen
             name="Home"
             component={HomeScreen}
+            
           />
-          <Tab.Screen name="Devnap" component={Devnap} />
+          <Tab.Screen name="Napkin" component={Devnap} />
           <Tab.Screen name="Bookmarks" component={Bookmarks} />
-          <Tab.Screen name="Job App Track" component={JobAdd} />
-          <Tab.Screen name="Dev List" component={AboutUs} />
+          <Tab.Screen name="Job Tracker" component={JobAdd} />
+          <Tab.Screen name="About the Devs" component={AboutUs} />
           
       
 
